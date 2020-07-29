5590c9aad952c454f3553577c112d711cd8887f0
Keegan Noble <keegannoble90@gmail.com>
2020-07-29 12:16:00 +0800

    added react and index js file with index view

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class ReviewsController < ApplicationController
+      protect_from_forgery with: :null_session
 
       def create
         review = Review.new(review_params)
@@ -13,7 +14,7 @@ module Api
       end
 
       def destroy
-        review = Review.find[params[:id]]
+        review = Review.find(params[:id])
 
         if review.destroy
           head :no_content
